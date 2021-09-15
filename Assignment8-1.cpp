ef54390616f155c6df0f65a05000af0da1930dc1
Jaspreet1720 <89234431+Jaspreet1720@users.noreply.github.com>
2021-09-14 18:55:22 -0700

    binary

--- Assignment8-1.cpp
+++ Assignment8-1.cpp
@@ -31,6 +31,19 @@ int binarySearch(int number[], int N, int target)
 	first =0;
     last = N-1;
     
+    while (first<=last)
+
+    mid = (first+last)/2;
+
+    if (numbers[mid]==target)
+    cout<<"found at index; "<< mid <<endl;
+
+    else if (numbers[mid]< target)
+    first = mid+1;
+    elselast = mid-1;
+    
 
 	return -1;
+
+
 }
