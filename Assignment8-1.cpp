731b91ff7283e22b1248dc94a1ac973ea5967bd4
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-09-17 19:10:58 +0000

    edited the algorithm

--- Assignment8-1.cpp
+++ Assignment8-1.cpp
@@ -1,44 +1,44 @@
 #include <iostream>
-#include <algorithm> 
-
+#include <algorithm> // needed to use the sort function.
 using namespace std;
 
+
 int binarySearch(int [], int, int);
+// 1st parameter : Integer Array
+// 2nd Parameter : Size of the array N
+// 3rd Parameter : Taget Value
+// Return value and type : the index of the array if the target is in the array. -1 if not found.
 
-int main() {
-  const int SIZE = 10;
-  int arr[SIZE] = {10, 25, 30, 35, 40, 45, 55, 60, 65, 70};
-  int target = 40;
-  int index = binarySearch(arr, SIZE, target);
 
-  if (index == -1) {
-    cout << "Target was not found..." << endl;
-    cout << "The target (" << target << ") does not exist in this array." << endl;
+int main()
+{
+	const int N = 10;
+	int numbers[N] = {10, 25, 30, 35, 40, 45, 55, 60, 65, 70 };
+	int target = 10;
+	int index;
 
-  }
-  else {
-    cout << "Target found!" << endl;
-    cout << "The target (" << target << ") exists in index #" << index << endl;
-  }
 
-  return 0;
+	index = binarySearch(numbers, N, target);
+	if ( index == -1)
+		cout << " not found \n";
+	else
+		cout << " found at index : " << index << endl;
 }
 
-int binarySearch(int arr[], int N, int target) {
-  int first = 0, last = N - 1, mid;
-  
-  while (first <= last) {
+int binarySearch(int number[], int N, int target)
+{
+	int first, last, mid;
+  first = 0;
+  last = N - 1;
+  while(first <= last){
     mid = (first + last) / 2;
-    if (arr[mid] == target) {
+    if(number[mid] == target)
       return mid;
-    }
-    else if (arr[mid] < target) {
+    else if(number[mid] < target)
       first = mid + 1;
-    } 
-    else if (arr[mid] > target){
+    else if(number[mid] > target)
       last = mid - 1;
-    }
   }
-
-  return -1;
+     
+	return -1;
 }
