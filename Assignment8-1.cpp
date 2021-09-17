63eb0169c184943a9b80b0697d5334a5dc6411f8
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-09-17 16:13:30 +0000

    updated the program

--- Assignment8-1.cpp
+++ Assignment8-1.cpp
@@ -1,49 +1,44 @@
 #include <iostream>
-#include <algorithm> // needed to use the sort function.
+#include <algorithm> 
+
 using namespace std;
 
-
 int binarySearch(int [], int, int);
-// 1st parameter : Integer Array
-// 2nd Parameter : Size of the array N
-// 3rd Parameter : Taget Value
-// Return value and type : the index of the array if the target is in the array. -1 if not found.
 
+int main() {
+  const int SIZE = 10;
+  int arr[SIZE] = {10, 25, 30, 35, 40, 45, 55, 60, 65, 70};
+  int target = 40;
+  int index = binarySearch(arr, SIZE, target);
 
-int main()
-{
-	const int N = 10;
-	int numbers[N] = {10, 25, 30, 35, 40, 45, 55, 60, 65, 70 };
-	int target = 35;
-	int index;
+  if (index == -1) {
+    cout << "Target was not found..." << endl;
+    cout << "The target (" << target << ") does not exist in this array." << endl;
 
+  }
+  else {
+    cout << "Target found!" << endl;
+    cout << "The target (" << target << ") exists in index #" << index << endl;
+  }
 
-	index = binarySearch(numbers, N, target );
-	if ( index == -1)
-		cout << " not found \n";
-	else
-		cout << " foudn at index : " << index << endl;
+  return 0;
 }
 
-int binarySearch(int number[], int N, int target)
-{
-	int first, last, mid;
-	first =0;
-    last = N-1;
-    
-    while (first<=last)
+int binarySearch(int arr[], int N, int target) {
+  int first = 0, last = N - 1, mid;
+  
+  while (first <= last) {
+    mid = (first + last) / 2;
+    if (arr[mid] == target) {
+      return mid;
+    }
+    else if (arr[mid] < target) {
+      first = mid + 1;
+    } 
+    else if (arr[mid] > target){
+      last = mid - 1;
+    }
+  }
 
-    mid = (first+last)/2;
-
-    if (numbers[mid]==target)
-    cout<<"found at index; "<< mid <<endl;
-
-    else if (numbers[mid]< target)
-    first = mid+1;
-    elselast = mid-1;
-    
-
-	return -1;
-
-
+  return -1;
 }
