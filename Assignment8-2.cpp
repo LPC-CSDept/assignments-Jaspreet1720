c6f0704cbf2d29b4595f48e662e9f01ffa7fde61
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-09-28 20:54:12 +0000

    Edited and compared linear and binary.

--- Assignment8-2.cpp
+++ Assignment8-2.cpp
@@ -4,83 +4,76 @@
 using namespace std;
 
 
-void makeArray(int [], int);
-void printArray(int [], int);
 int linearSearch(int [], int , int );
+int binarySearch(int [], int , int );
+void makeArray(int [] , int);
+void printArray(int [] , int);
+void bubbleSort(int [], int );
+// below it shows the comaparison of linear and binary search algorithms
 
 int main()
-{
-	const int TRY = 10000;
-	const int N = 64;
-	int array[N];
-	int target;
-	int total_linear = 0;
+{  
+    const int N= 64;
+    int array[N];
+    int target;
+    int total_linear, total_binary;
 
-	srand(time(0)); // set the seed number to random function
-	makeArray(array, N);
-	printArray(array, N);
+    srand(time(0));
+    makeArray(array, N);
+    printArray(array, N);
 
-	for(int i=0; i<TRY; i++){
-		target = array[rand() % 64];
-		total_linear += linearSearch(array, N, target);
-	}
-	cout << "The average comparison number " << total_linear / TRY << endl;
-	
-	// Sort
-	// selectSort(array, N);
-	
-	
-	for(int i=0; i<TRY; i++){
-		target = array[rand() % 64];
-		total_linear += binarySearch(array, N, target);
-	}
-	cout << "The average comparison number " << total_linear / TRY << endl;
+    for(int i=0; i<100; i++)
+    {
+        target = array[rand() % N];
+        total_linear += linearSearch(array, N, target) ;
+    }
+    cout << "The average comparision numbers of Linear Search is " << total_linear / 100.0 << endl;
 
+    bubbleSort(array, N);
+    printArray(array, N);
+    for(int i=0; i<100; i++)
+    {
+        target = array[rand() % N];
+        total_binary += binarySearch(array, N, target) ;
+    }
+    cout << "The average comparision numbers of Binary Search is " << total_binary / 100.0 << endl;
+    
 
-}
-
-
-int linearSearch(int array[], int N, int target)
-{
-    for(int i=0; i<N; i++)
-        if ( array[i] == target)
-            return i+1;
-    return N;
 }
 
 void makeArray(int array[], int N)
 {
-	for(int i=0; i<N; i++)
-		array[i] = rand() % 100 +1 ;  // random numbers between 1 to 100
-
+    for(int i=0; i<N; i++)
+        array[i] = rand() % 100 + 1;
 }
-
 void printArray(int array[], int N)
 {
-    for(int i=0; i<N; i++)
+    for(int i=0; i<100; i++)
     {
         cout << array[i]  << "\t" ; 
         if (i % 8 == 7 ) cout << endl;
     }
 }
 
+void bubbleSort(int array[], int N)
+{
+	int min, index;
+	for(int i=0; i < N-1; i++)
+	{
+		for(int j=0; j<N-i-1; j++)
+		{
+			if ( array[j] > array[j+1])
+				swap(array[j], array[j+1]);
+		}
+	}
+}
+
+int linearSearch(int array[], int N, int target)
+{
+   
+}
+
 int binarySearch(int array[], int N, int target)
 {
-    int first, last, mid;
-    int cmp = 0;
-
-    first = 0;
-    last = N-1;
-    while ( first <= last) 
-    {
-		cmp += 1;
-        mid = (first + last) / 2;
-        if ( array[mid] == target)
-            return cmp;
-        if ( array[mid] > target)
-            last = mid - 1;
-        else 
-            first = mid + 1;
-    }
-    return cmp;
+ 
 }
