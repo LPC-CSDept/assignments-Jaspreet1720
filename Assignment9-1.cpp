440cf52c37b9d0d907233450d327223d92736522
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-09-29 01:54:58 +0000

    Access the array values

---
+++ Assignment9-1.cpp
@@ -0,0 +1,20 @@
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+
+	// int 	num[5] = {10, 20, 30, 40, 50};
+	int 	num1, num2, num3, num4, num5;
+	num1 = 10;
+	num2 = 20;
+	num3 = 30;
+	num4 = 40;
+	num5 = 50;
+
+	int 	* ptr;
+	ptr =  &num1;
+
+	cout << *ptr << endl; // print value? 10
+	cout << *(ptr+1) << endl; // print value? 20 
+}
