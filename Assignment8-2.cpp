2e4259bb8774a9947028b5edd4b9eeccd72f225e
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-09-30 22:50:03 +0000

    updated program with linear and binary, also replaced 100 with N.

--- Assignment8-2.cpp
+++ Assignment8-2.cpp
@@ -22,21 +22,21 @@ int main()
     makeArray(array, N);
     printArray(array, N);
 
-    for(int i=0; i<100; i++)
+    for(int i=0; i<N; i++)
     {
         target = array[rand() % N];
         total_linear += linearSearch(array, N, target) ;
     }
-    cout << "The average comparision numbers of Linear Search is " << total_linear / 100.0 << endl;
+    cout << "The average comparision numbers of Linear Search is " << total_linear / N << endl;
 
     bubbleSort(array, N);
     printArray(array, N);
-    for(int i=0; i<100; i++)
+    for(int i=0; i<N; i++)
     {
         target = array[rand() % N];
         total_binary += binarySearch(array, N, target) ;
     }
-    cout << "The average comparision numbers of Binary Search is " << total_binary / 100.0 << endl;
+    cout << "The average comparision numbers of Binary Search is " << total_binary / N << endl;
     
 
 }
@@ -44,11 +44,11 @@ int main()
 void makeArray(int array[], int N)
 {
     for(int i=0; i<N; i++)
-        array[i] = rand() % 100 + 1;
+        array[i] = rand() % N + 1;
 }
 void printArray(int array[], int N)
 {
-    for(int i=0; i<100; i++)
+    for(int i=0; i<N; i++)
     {
         cout << array[i]  << "\t" ; 
         if (i % 8 == 7 ) cout << endl;
@@ -70,10 +70,36 @@ void bubbleSort(int array[], int N)
 
 int linearSearch(int array[], int N, int target)
 {
-   
+   for(int i = 0; i<N; i++)
+
+   if(array[i] == target)
+      return i+1;
+
+      return N;
 }
 
 int binarySearch(int array[], int N, int target)
 {
- 
+ int first, last, mid;
+
+ int cmp = 0;
+
+ first = 0;
+
+ last = N-1;
+ while(first <= last)
+ {
+   cmp += 1;
+   mid = (first + last)/2;
+
+   if( array[mid] == target)
+   return cmp;
+
+   if(array[mid] > target)
+   last = mid - 1;
+
+   else
+   first = mid + 1;
+ }
+ return cmp;
 }
