80563682e4db15e6dde03862d0609484b582c7f8
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-10-22 03:41:52 +0000

    Need to take a look again

---
+++ Constructor-rect1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+private:
+	int width;
+	int height;
+
+public:
+	Rectangle() : width(0), height(0) { } //initialization
+ 	int getWidth() const { return width; };
+	int getHeight() const { return height; };
+	int getArea() const { return width * height; };
+	void setWidth(int w) { width = w; };
+	void setHeight(int h) { height = h; };
+};
+
+int main()
+{
+	Rectangle r1; // constructor is called automatically
+	
+	cout << "W " << r1.getWidth() << endl;
+	cout << "H " << r1.getHeight() << endl;
+
+}
