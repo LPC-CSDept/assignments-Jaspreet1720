dbb09ec27447518ef5016a8c3b7b19cb97be80f9
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-10-29 02:07:33 +0000

    Code Completed

---
+++ A13-4.cpp
@@ -0,0 +1,19 @@
+#include "Stack.hpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int numbers[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+	Stack	s(10);
+
+	for( int i=0; i < 10; i++)
+		s.push(numbers[i]);
+	s.printAll();
+	for( int i=0; i < 10; i++)
+		numbers[i] = s.pop(); 
+		
+	for( int i=0; i < 10; i++)
+		cout << numbers[i] << endl;
+
+}
--- Assignment13-2/A13-2.cpp
+++ Assignment13-2/A13-2.cpp
@@ -23,8 +23,4 @@ int main()
 
 	n2.printOut();
 
-
-
-
-	
 }
