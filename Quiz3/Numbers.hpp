8b7ada72c30eb173acf6c744fefae21ea6c97210
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-11-03 12:25:02 +0000

    hpp file done

---
+++ Quiz3/Numbers.hpp
@@ -0,0 +1,25 @@
+#ifndef NUMBERS_H
+#define NUMBERS_H
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Numbers 
+{
+private:
+       int ID;
+       vector<int> numbers;
+
+public:
+      Numbers();
+      Numbers(int, int);
+      int getID() const;
+      int getSize() const;
+      int getElm(int) const;
+      int getMax() const;
+      int getMin() const;
+      int getSum() const;
+      void deleteElm(int);
+};
+
+#endif
