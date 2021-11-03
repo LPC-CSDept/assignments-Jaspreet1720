d5959117d7a5af31fb8d782b6d9b92778246d44e
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-11-03 12:42:26 +0000

    still working

--- Quiz3/Numbers.cpp
+++ Quiz3/Numbers.cpp
@@ -1,2 +1,31 @@
 #include "Numbers.hpp"
 
+Numbers::Numbers()
+{
+  ID = -1; // set the id as -1
+}
+
+Numbers::Numbers(int id, int size)
+{
+  ID = id; //  set the id
+  for(int i=0; i <size; i++)
+  { 
+    int x = rand() % 100; // generate an integer
+    numbers.push_back(x); // push it back to the vector
+ }
+}
+
+int Numbers::getID() const 
+{
+  return ID;
+}
+
+int Numbers::getSize() const
+{
+  return numbers.size();
+}
+
+int NUmbers::getElm(int i) const
+{
+  return numbers.at(i);
+}
