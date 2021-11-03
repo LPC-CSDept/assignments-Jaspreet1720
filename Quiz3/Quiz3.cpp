ca3de6dabb576878fdf42edab5f50d01c62ca858
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-11-03 13:37:39 +0000

    still on it

--- Quiz3/Quiz3.cpp
+++ Quiz3/Quiz3.cpp
@@ -19,4 +19,42 @@ int findMax(Numbers *numberset, int size)
   return max_ID;
 }
 
+void deDup(Numbers &numberset1, Numbers &numberset2)
+{
+  vector<int> duplicates;
+  for(int i = 0; i < numberset1.getSize(); i++)
+  {
+    for(int j = 0; j < numberset2.getSize(); j++)
+    {
+      if(numberset1.getElm(i) == numberset2.getElm(i))
+      {
+        duplicates.push_back(numberset1getElm(i)) break;
+      }
+    }
 
+  }
+
+  for(int i = 0; i < duplicates.size(); i++)
+  {
+    numberset1.deleteElm(duplicates.at(i));
+    numberset2.deleteElm(duplicates.at(i));
+  }
+}
+
+void printNumbers(Numbers &n)
+{
+  for(int i = 0; i < n.getSize(); i++)
+  {
+    cout << n.getElm(i) << " " ;
+  }
+  cout << endl;
+}
+
+void printNumberSet(Numbers *numberset, int size)
+{
+  for(int j = 0; j < size; j++)
+  {
+    printNumbers(numberset[j]);
+  }
+  cout << endl;
+}
