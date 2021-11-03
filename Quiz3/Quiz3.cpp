d67c86b3f30160bcbf4835e909fd5494ddd9a1b5
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-11-03 13:24:18 +0000

    working on it

--- Quiz3/Quiz3.cpp
+++ Quiz3/Quiz3.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+#include <time.h>
+#include "Numbers.hpp"
+
+int findMax(Numbers *numberset, int size)
+{
+  int max_diff = -1;
+  int max_ID;
+
+  for(int i = 0; i < size; i++)
+  {
+    int curr_diff = numberset[i].getMax() - numberset[i].getMin();
+    if(curr_diff > max_diff)
+    {
+      max_diff = curr_diff;
+      max_ID = numberset[i].getID();
+    }
+  }
+  return max_ID;
+}
+
+
