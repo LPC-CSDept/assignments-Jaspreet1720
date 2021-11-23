9e7e4ed92c48d5d2ab2e308f478a9bf468b43512
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-11-23 04:22:58 +0000

    working on student.cpp

--- Quiz-4/Student.cpp
+++ Quiz-4/Student.cpp
@@ -2,3 +2,13 @@
 #include <string>
 
 using namespace std;
+
+Student::Student() {}
+Student::Student(int ID, string sname, char grade, double scores)
+{
+  this->ID = ID;
+  this->sname = sname;
+  this->grade = grade;
+  this->scores = scores;
+}
+
