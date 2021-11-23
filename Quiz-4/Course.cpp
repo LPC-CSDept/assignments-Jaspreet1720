7a31ed43eae9bdb5b645251fa2cf617dd8b9d7e2
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-11-23 03:48:07 +0000

    on it

--- Quiz-4/Course.cpp
+++ Quiz-4/Course.cpp
@@ -1,1 +1,15 @@
 #include "Course.hpp"
+#include "Student.hpp"
+#include <cstring>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int Course::NUM_COURSES = 0;
+
+Course::Course()
+{
+  NUM_COURSES += 1;
+}
--- Quiz-4/Course.hpp
+++ Quiz-4/Course.hpp
@@ -4,3 +4,22 @@
 #include <fstream>
 #include <vector>
 #include <string>
+
+using namespace std;
+
+class Student;
+
+class Course
+{
+  private:
+
+  string cname;
+  int credits;
+  string semester;
+  vector<Student> student;
+  static int NUM_COURSES;
+
+  public:
+  Course();
+  Course(string, int, string, vector<Student>);
+
