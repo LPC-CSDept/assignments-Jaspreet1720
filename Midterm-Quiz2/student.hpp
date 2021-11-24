28493875d9d5297a3aacf64759e7f1bbc3c78e1a
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-11-24 21:14:07 +0000

    done with hpp file

--- Midterm-Quiz2/student.hpp
+++ Midterm-Quiz2/student.hpp
@@ -35,4 +35,28 @@ class student
     }
   }
 
-}
+  int binarysearch(student a[], int n, int id)
+  {
+    int start = 0;
+    int end = n-1;
+
+    while(start<=end)
+    {
+      int mid = (start+end)/2;
+
+      if(a[mid].id == id)
+      {
+        return mid;
+      }
+      else if(a[mid].id >id)
+      {
+        start = mid-1;
+      }
+      else{
+        end = mid - 1;
+      }
+    }
+    return -1;
+  }
+
+};
