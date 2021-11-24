c263aee139eb868c3bec64916f83644d622acf23
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-11-24 21:01:23 +0000

    :( confused now

--- Midterm-Quiz2/student.hpp
+++ Midterm-Quiz2/student.hpp
@@ -1,3 +1,31 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+class student
+{
+  int id;
+  string name;
+  int score1;
+  int score2;
+  int score3;
+  int total scores;
+
+  void display(student a[], int n)
+  {
+    for(int i = 0; i < n; i++)
+    {
+      int swaps = 0;
+
+      for(int j = 0; j < n-i-1; j++)
+      {
+        if(a[j].total_scores < a[j+1].total_scores)
+        {
+          swap(a[j], a[j+1]);
+          swaps = 1;
+        }
+      }
+    }
+  }
+
+}
