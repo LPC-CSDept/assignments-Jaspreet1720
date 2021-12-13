3d63a5775ddb559c33f9252140d7f0ceccea214a
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-12-13 21:37:59 +0000

    file is completed using getter method

--- Final_Quiz1/Course.cpp
+++ Final_Quiz1/Course.cpp
@@ -11,3 +11,38 @@ Course::Course()
 
 Course::Course(int id, string name, int credit) : id(id), name(name), credit(credit){}
 
+// used getter method
+int Course::getId() const
+{
+  return id;
+}
+
+const string &Course::getName() const
+{
+  return name;
+}
+
+int Course::getCredit() const
+{
+  return credit;
+}
+
+void Course::setId(int id)
+{
+  this->id = id;
+}
+
+void Course::setName(const string &name)
+{
+  this->name = name;
+}
+
+void Coure::setCredit(int credit)
+{
+  this->credit = credit;
+}
+
+void Course::print() const 
+{
+  cout << "Course{id =" << id << ", name = " << name << ", credit = "<< credit << "}" << endl;
+}
