845249a6d7f7dd08abf0192bafc243ae5cdc4338
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-12-13 21:24:38 +0000

    on it

--- Final_Quiz1/Course.cpp
+++ Final_Quiz1/Course.cpp
@@ -1,3 +1,13 @@
 #include "Course.hpp"
 #include <iostream>
 using namespace std;
+
+Course::Course()
+{
+  id = 0;
+  name = " ";
+  credit = 0;
+}
+
+Course::Course(int id, string name, int credit) : id(id), name(name), credit(credit){}
+
