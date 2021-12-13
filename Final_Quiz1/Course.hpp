814198d8d525d42170944cde22a8900ce75918cd
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-12-13 21:03:02 +0000

    on it

--- Final_Quiz1/Course.hpp
+++ Final_Quiz1/Course.hpp
@@ -3,3 +3,13 @@
 #include <iostream>
 
 using namespace std;
+
+class Course
+{
+  private:
+  int id; // its a data member to store the course id
+  string name; // its a data member to store the course name
+  int credit; // its a data member to store the course credit
+
+
+}
