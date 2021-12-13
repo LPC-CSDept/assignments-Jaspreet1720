bed439addb38f9e34f5a8427162e31a7bf97498c
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-12-13 21:48:37 +0000

    working on it

--- Final_Quiz1/main.cpp
+++ Final_Quiz1/main.cpp
@@ -3,3 +3,18 @@
 #include "Course.hpp"
 
 using namespace std;
+
+int binary_search(Course[], int, int);
+int recursive_binary_search(Course[], int, int, int);
+
+int main()
+{
+  ifstream data_file("data.txt"); // will open the file containing the input data
+
+  if(data_file.fail())
+  {
+    cout << "Error!! Failed to open data.txt file..."<< endl;
+    Cout << "Make sure data.txt file is present in the current working directory." << endl;
+    return 1;
+  }
+}
