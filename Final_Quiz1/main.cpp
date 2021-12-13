d171c312b49495900f4e2c59b51915ec6571c170
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-12-13 21:58:39 +0000

    still working on it

--- Final_Quiz1/main.cpp
+++ Final_Quiz1/main.cpp
@@ -17,4 +17,38 @@ int main()
     Cout << "Make sure data.txt file is present in the current working directory." << endl;
     return 1;
   }
-}
+
+  Course courses[10];
+  int n = 0;
+
+  while(!data_file.eof())
+  {
+    int id, credit;
+    string name;
+
+    data_file >> id;
+    data_file >> name;
+    data_file >> credit;
+
+    Course c(id, name, credit);
+
+    courses[n++] = c;
+  }
+
+  data_file.close();
+
+  for(int p = 0; p < n; p++)
+  {
+    for(int q = 0; q < n - p - 1; q++)
+    {
+      if(courses[q].getId() > courses[q+1].getId())
+      {
+        Course c  = course[q];
+        courses[q] = courses[q+1];
+        courses[q+1] = c;
+      }
+    }
+  }
+
+
+}  
