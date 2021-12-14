fe5f89336d62b92189588bd425b43bf43637aceb
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-12-14 00:26:47 +0000

    on it

--- Final_Quiz2/main.cpp
+++ Final_Quiz2/main.cpp
@@ -8,5 +8,46 @@ void quick_sort(Course *, int, int);
 
 int main()
 {
-  
+  ifstream data_file("data.txt");
+  if(data_file.fail())
+  {
+    cout << "Error!! Failed to open data.txt file..." << endl;
+    return 1;
+  }
+
+  Course courses[10];
+  int n = 0;
+
+  while(!data_file.eof())
+  {
+    int id, credit;
+    string name;
+
+    data_file >> id;
+    data_file >> name;
+    data_file >> credit;
+
+    Course c(id, name, credit);
+    courses[n++] = c;
+  }
+
+  data_file.close();
+
+  cout << "Array Before Sorting: " << endl;
+
+  for(int i = 0; i < n; i++)
+  {
+    courses[i].print();
+  }
+
+  quick_sort(courses, 0, n-1);
+
+  cout << "\nArray After Performing the Quick Sort: " << endl;
+
+  for(int i = 0; i < n, i++)
+  {
+    courses[i],print();
+  }
+    return 0;
+
 }
