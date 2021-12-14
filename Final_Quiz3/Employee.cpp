144cf782254a243ba77e84282f5565613231f48c
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-12-14 01:39:59 +0000

    still on it

--- Final_Quiz3/Employee.cpp
+++ Final_Quiz3/Employee.cpp
@@ -2,3 +2,38 @@
 #include <fstream>
 
 using namespace std;
+
+class Employee
+{
+  private:
+
+  string emp_name, emp_number;
+  string hire_date;
+
+  public:
+      Employee()
+      {
+        emp_name = " ";
+        emp_number= " ";
+        hire_date = " ";
+      }
+
+      Employee(string name, string e_number, string date)
+      {
+        emp_name = " ";
+        emp_number = " ";
+        hire_date = " ";
+      }
+
+      string get_emp_name(){
+        return emp_name;
+      }
+
+      string get_emp_number(){
+        return emp_number;
+      }
+
+      string get_hire_date(){
+        return hire_date;
+      }
+};
