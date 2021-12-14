9138f40add20e2afcfff84eaaac2d79a862fd48b
jsaujla17 <89234431+Jaspreet1720@users.noreply.github.com>
2021-12-14 00:10:26 +0000

    Hpp file is same as Quiz 1

---
+++ Final_Quiz2/Course.hpp
@@ -0,0 +1,27 @@
+#ifndef COURSE_H
+#define COURSE_H
+#include <iostream>
+
+using namespace std;
+
+class Course
+{
+  private:
+  int id; // its a data member to store the course id
+  string name; // its a data member to store the course name
+  int credit; // its a data member to store the course credit
+
+  public:
+
+  Course(); // its a default constructor
+  Course(int id, string name, int credit);//its a parameterized constructor
+  int getId() const;//Will return the course id
+  const string &getName() const; // will return the course name
+  int getCredit() const;// will return the course credit
+  void setId(int id);//it sets the course id
+  void setName(const string &name);//it sets the course name
+  void setCredit(int credit); //it sets the course credit
+  void print() const;//it will display the course details
+};
+
+#endif
